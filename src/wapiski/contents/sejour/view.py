cd7965619ecd6d6890e5097eedc10c9951a05493
FBruynbroeck <francois.bruynbroeck@hotmail.com>
2020-08-18 12:31:30 +0200

    Add cal_date in listing sejour/stage (like event_listing)

--- src/wapiski/contents/sejour/view.py
+++ src/wapiski/contents/sejour/view.py
@@ -1,4 +1,6 @@
 # encoding: utf-8
+from plone.app.contenttypes.browser.collection import CollectionView
+from plone.app.event.base import spell_date
 from plone.dexterity.browser.view import DefaultView
 from zope.component import getMultiAdapter
 from zope.contentprovider.interfaces import IContentProvider
@@ -6,14 +8,15 @@ from zope.contentprovider.interfaces import IContentProvider
 
 class SejourView(DefaultView):
 
-    def lf_to_p(self, text):
-        if not text:
-            return u"<p></p>"
-        return u"<p>" + text.replace(u'\n', u'</p><p>') + u"</p>"
-
     def formatted_date(self, item):
         provider = getMultiAdapter(
             (self.context, self.request, self),
             IContentProvider, name='formatted_date'
         )
         return provider(item)
+
+
+class SejourCollection(CollectionView):
+
+    def date_speller(self, date):
+        return spell_date(date, self.context)
